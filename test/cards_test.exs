aa38ad5746c4bbd44d1dad81afc68d2d31fc7293
jbarget <dear.justen@gmail.com>
2017-02-28 11:01:06 +0200

    adding tests for create_deck, shuffle and create_hand

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,19 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "create_deck makes a deck of 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 40
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "create_hand starts a game of cards" do
+    {hand, deck} = Cards.create_hand(2)
+    assert length(hand) == 2
+    assert length(deck) == 38
   end
 end
